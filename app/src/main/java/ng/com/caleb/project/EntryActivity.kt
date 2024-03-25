32502afb14d4edddf09d6767800fe4c2fa12fe47
Oluwatobiloba Johnson <toby.johnson.precious@gmail.com>
2024-03-25 02:11:53 +0100

    Added the required Firebase methods

---
+++ app/src/main/java/ng/com/caleb/project/EntryActivity.kt
@@ -0,0 +1,15 @@
+package ng.com.caleb.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+/**
+ * This is the entry point of our application. What it does is just check if any user is authenticated
+ * and then redirects them to either the authentication screen or the main activity.
+ */
+class EntryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
